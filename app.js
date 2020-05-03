9c933f5c2c5d26e84911a02ac83a18b1a77d3052
bpartyka <basiapart@gmail.com>
2020-05-03 15:31:02 +0200

    add point highlight

--- app.js
+++ app.js
@@ -6,17 +6,20 @@ const harpunBody = document.body;
 const harpun = document.querySelector('.container');
 const title = document.querySelector('.title');
 
+var currentSelected;
+var vectorLayer;
+
 function jedenNumer(e) {
     var m = [];
-    var mag =[];
+    var mag = [];
 
     const xhr = new XMLHttpRequest();
-    
+
     xhr.open('GET', 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson', false);
-    
-    xhr.onload = function() {
+
+    xhr.onload = function () {
         const response = JSON.parse(this.responseText);
-    
+
         response.features.forEach(i => {
             let x = i.geometry.coordinates[0];
             let y = i.geometry.coordinates[1];
@@ -28,7 +31,7 @@ function jedenNumer(e) {
 
         return m;
     };
-    
+
     xhr.send();
 
     let h = document.createElement('h3');
@@ -45,7 +48,10 @@ function jedenNumer(e) {
     p.id = "app";
     p.innerHTML = `
     <ul>
-        <li v-for="i in info">Place: <b>{{ i.properties.place }}</b><br>Magnitude: <b>{{ i.properties.mag }} Richter scale</b></li>
+        <li v-for="i in info" >
+            <span :id="i.geometry.coordinates[0]"> 
+                Place: <b>{{ i.properties.place }}</b><br>Magnitude: <b>{{ i.properties.mag }} Richter scale</b></li>
+            </span>
     </ul>
     <button class="legend">View legend</button>
     `;
@@ -54,7 +60,7 @@ function jedenNumer(e) {
     view();
 
     document.querySelector('.legend').addEventListener('click', legenda);
-    
+
 
     let b = document.createElement('button');
     b.className = "siedem btn btn-lg btn-primary btn-block";
@@ -66,37 +72,47 @@ function jedenNumer(e) {
     harpunBody.removeChild(harpun);
     harpunBody.removeChild(title);
 
-    
+
     map = new OpenLayers.Map("mapdiv");
     map.addLayer(new OpenLayers.Layer.OSM());
-    
-    epsg4326 =  new OpenLayers.Projection("EPSG:4326"); //WGS 1984 projection
+
+    epsg4326 = new OpenLayers.Projection("EPSG:4326"); //WGS 1984 projection
     projectTo = map.getProjectionObject(); //The map projection (Spherical Mercator)
-    
-    var lonLat = new OpenLayers.LonLat( 50.0 ,100.0 ).transform(epsg4326, projectTo);
-          
-    var zoom=2;
-    map.setCenter (lonLat, zoom);
-    
-    var vectorLayer = new OpenLayers.Layer.Vector("Overlay");
-    
+
+    var lonLat = new OpenLayers.LonLat(50.0, 100.0).transform(epsg4326, projectTo);
+
+    var zoom = 2;
+    map.setCenter(lonLat, zoom);
+
+    vectorLayer = new OpenLayers.Layer.Vector("Overlay");
+
     //Loop through the markers array
-    for (var i=0; i<m.length; i++) {
-      
-       var lon = m[i][0];
-       var lat = m[i][1];
-       var rad = m[i][2]
-       var magnitude = Math.min(Math.max(mag[i] *5, 15), 35) ;
+    for (var i = 0; i < m.length; i++) {
+
+        var lon = m[i][0];
+        var lat = m[i][1];
+        var rad = m[i][2]
+        var magnitude = Math.min(Math.max(mag[i] * 5, 15), 35);
 
         var feature = new OpenLayers.Feature.Vector(
-                new OpenLayers.Geometry.Point( lon, lat ).transform(epsg4326, projectTo),
-                {description: "marker number " + i} ,
-                {externalGraphic: 'img/marker.png', graphicHeight: magnitude, graphicWidth: magnitude, graphicXOffset:-12, graphicYOffset:-25  }
-            );             
+            new OpenLayers.Geometry.Point(lon, lat).transform(epsg4326, projectTo),
+            { description: "marker number " + i, lon: lon, lat: lat, magnitude: magnitude },
+            { externalGraphic: 'img/marker.png', graphicHeight: magnitude, graphicWidth: magnitude, graphicXOffset: -12, graphicYOffset: -25 }
+        );
         vectorLayer.addFeatures(feature);
-    }                        
-    
+    }
+
     map.addLayer(vectorLayer);
+
+    selectFeature = new OpenLayers.Control.SelectFeature(
+        vectorLayer,
+        {
+            onSelect: onFeatureSelect,
+            onUnselect: onFeatureUnselect,
+            autoActivate: true
+        }
+    );
+    map.addControl(selectFeature);
 }
 
 function dwaNumer(e) {
@@ -104,24 +120,23 @@ function dwaNumer(e) {
     var mag = [];
 
     const xhr = new XMLHttpRequest();
-    
+
     xhr.open('GET', 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_week.geojson', false);
-    
-    xhr.onload = function() {
+
+    xhr.onload = function () {
         const response = JSON.parse(this.responseText);
-       
+
         response.features.forEach(i => {
             let x = i.geometry.coordinates[0];
             let y = i.geometry.coordinates[1];
-    
-            m.push([x, y]);
 
+            m.push([x, y]);
             mag.push(i.properties.mag);
         });
 
         return m;
     };
-    
+
     xhr.send();
 
     let h = document.createElement('h3');
@@ -138,7 +153,10 @@ function dwaNumer(e) {
     p.id = "app";
     p.innerHTML = `
     <ul>
-        <li v-for="i in info1">Place: <b>{{ i.properties.place }}</b><br>Magnitude: <b>{{ i.properties.mag }} Richter scale</b></li>
+        <li v-for="i in info1" >
+            <span :id="i.geometry.coordinates[0]"> 
+                Place: <b>{{ i.properties.place }}</b><br>Magnitude: <b>{{ i.properties.mag }} Richter scale</b></li>
+            </span>
     </ul>
     <button class="legend">View legend</button>
     `;
@@ -147,7 +165,7 @@ function dwaNumer(e) {
     view1();
 
     document.querySelector('.legend').addEventListener('click', legenda);
-    
+
 
     let b = document.createElement('button');
     b.className = "siedem";
@@ -159,36 +177,46 @@ function dwaNumer(e) {
     harpunBody.removeChild(harpun);
     harpunBody.removeChild(title);
 
-    
+
     map = new OpenLayers.Map("mapdiv");
     map.addLayer(new OpenLayers.Layer.OSM());
-    
-    epsg4326 =  new OpenLayers.Projection("EPSG:4326"); //WGS 1984 projection
+
+    epsg4326 = new OpenLayers.Projection("EPSG:4326"); //WGS 1984 projection
     projectTo = map.getProjectionObject(); //The map projection (Spherical Mercator)
-    
-    var lonLat = new OpenLayers.LonLat( 50.0 ,100.0 ).transform(epsg4326, projectTo);
-          
-    var zoom=2;
-    map.setCenter (lonLat, zoom);
-    
-    var vectorLayer = new OpenLayers.Layer.Vector("Overlay");
-    
+
+    var lonLat = new OpenLayers.LonLat(50.0, 100.0).transform(epsg4326, projectTo);
+
+    var zoom = 2;
+    map.setCenter(lonLat, zoom);
+
+    vectorLayer = new OpenLayers.Layer.Vector("Overlay");
+
     //Loop through the markers array
-    for (var i=0; i<m.length; i++) {
-      
-       var lon = m[i][0];
-       var lat = m[i][1];
-       var magnitude = Math.min(Math.max(mag[i] *5, 15), 35) ;
+    for (var i = 0; i < m.length; i++) {
+
+        var lon = m[i][0];
+        var lat = m[i][1];
+        var magnitude = Math.min(Math.max(mag[i] * 5, 15), 35);
 
         var feature = new OpenLayers.Feature.Vector(
-                new OpenLayers.Geometry.Point( lon, lat ).transform(epsg4326, projectTo),
-                {description: "marker number " + i} ,
-                {externalGraphic: 'img/marker.png', graphicHeight: magnitude, graphicWidth: magnitude, graphicXOffset:-12, graphicYOffset:-25  }
-            );             
+            new OpenLayers.Geometry.Point(lon, lat).transform(epsg4326, projectTo),
+            { description: "marker number " + i, lon: lon, lat: lat, magnitude: magnitude },
+            { externalGraphic: 'img/marker.png', graphicHeight: magnitude, graphicWidth: magnitude, graphicXOffset: -12, graphicYOffset: -25 }
+        );
         vectorLayer.addFeatures(feature);
-    }                        
-    
+    }
+
     map.addLayer(vectorLayer);
+
+    selectFeature = new OpenLayers.Control.SelectFeature(
+        vectorLayer,
+        {
+            onSelect: onFeatureSelect,
+            onUnselect: onFeatureUnselect,
+            autoActivate: true
+        }
+    );
+    map.addControl(selectFeature);
 }
 
 function trzyNumer(e) {
@@ -196,10 +224,10 @@ function trzyNumer(e) {
     var mag = [];
 
     const xhr = new XMLHttpRequest();
-    
+
     xhr.open('GET', 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson', false);
-    
-    xhr.onload = function() {
+
+    xhr.onload = function () {
         const response = JSON.parse(this.responseText);
 
         response.features.forEach(i => {
@@ -212,7 +240,7 @@ function trzyNumer(e) {
 
         return m;
     };
-    
+
     xhr.send();
 
     let h = document.createElement('h3');
@@ -229,7 +257,10 @@ function trzyNumer(e) {
     p.id = "app";
     p.innerHTML = `
     <ul>
-        <li v-for="i in info2">Place: <b>{{ i.properties.place }}</b><br>Magnitude: <b>{{ i.properties.mag }} Richter scale</b></li>
+        <li v-for="i in info2" >
+            <span :id="i.geometry.coordinates[0]"> 
+                Place: <b>{{ i.properties.place }}</b><br>Magnitude: <b>{{ i.properties.mag }} Richter scale</b></li>
+            </span>
     </ul>
     <button class="legend">View legend</button>
     `;
@@ -238,7 +269,7 @@ function trzyNumer(e) {
     view2();
 
     document.querySelector('.legend').addEventListener('click', legenda);
-    
+
 
     let b = document.createElement('button');
     b.className = "siedem";
@@ -250,36 +281,46 @@ function trzyNumer(e) {
     harpunBody.removeChild(harpun);
     harpunBody.removeChild(title);
 
-    
+
     map = new OpenLayers.Map("mapdiv");
     map.addLayer(new OpenLayers.Layer.OSM());
-    
-    epsg4326 =  new OpenLayers.Projection("EPSG:4326"); //WGS 1984 projection
+
+    epsg4326 = new OpenLayers.Projection("EPSG:4326"); //WGS 1984 projection
     projectTo = map.getProjectionObject(); //The map projection (Spherical Mercator)
-    
-    var lonLat = new OpenLayers.LonLat( 50.0 ,100.0 ).transform(epsg4326, projectTo);
-          
-    var zoom=2;
-    map.setCenter (lonLat, zoom);
-    
-    var vectorLayer = new OpenLayers.Layer.Vector("Overlay");
-    
+
+    var lonLat = new OpenLayers.LonLat(50.0, 100.0).transform(epsg4326, projectTo);
+
+    var zoom = 2;
+    map.setCenter(lonLat, zoom);
+
+    vectorLayer = new OpenLayers.Layer.Vector("Overlay");
+
     //Loop through the markers array
-    for (var i=0; i<m.length; i++) {
-      
-       var lon = m[i][0];
-       var lat = m[i][1];
-       var magnitude = Math.min(Math.max(mag[i] *5, 15), 35) ;
+    for (var i = 0; i < m.length; i++) {
+
+        var lon = m[i][0];
+        var lat = m[i][1];
+        var magnitude = Math.min(Math.max(mag[i] * 5, 15), 35);
 
         var feature = new OpenLayers.Feature.Vector(
-                new OpenLayers.Geometry.Point( lon, lat ).transform(epsg4326, projectTo),
-                {description: "marker number " + i} ,
-                {externalGraphic: 'img/marker.png', graphicHeight: magnitude, graphicWidth: magnitude, graphicXOffset:-12, graphicYOffset:-25  }
-            );             
+            new OpenLayers.Geometry.Point(lon, lat).transform(epsg4326, projectTo),
+            { description: "marker number " + i, lon: lon, lat: lat, magnitude: magnitude },
+            { externalGraphic: 'img/marker.png', graphicHeight: magnitude, graphicWidth: magnitude, graphicXOffset: -12, graphicYOffset: -25 }
+        );
         vectorLayer.addFeatures(feature);
-    }                        
-    
+    }
+
     map.addLayer(vectorLayer);
+
+    selectFeature = new OpenLayers.Control.SelectFeature(
+        vectorLayer,
+        {
+            onSelect: onFeatureSelect,
+            onUnselect: onFeatureUnselect,
+            autoActivate: true
+        }
+    );
+    map.addControl(selectFeature);
 }
 
 function odswiezanie() {
@@ -289,15 +330,15 @@ function odswiezanie() {
 function view() {
     new Vue({
         el: '#app',
-        data () {
+        data() {
             return {
                 info: null
             }
         },
-        mounted () {
+        mounted() {
             axios
                 .get('https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson')
-                .then(response => ( this.info = response.data.features ))
+                .then(response => (this.info = response.data.features))
         }
     })
 }
@@ -305,15 +346,15 @@ function view() {
 function view1() {
     new Vue({
         el: '#app',
-        data () {
+        data() {
             return {
                 info1: null
             }
         },
-        mounted () {
+        mounted() {
             axios
                 .get('https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_week.geojson')
-                .then(response => ( this.info1 = response.data.features ))
+                .then(response => (this.info1 = response.data.features))
         }
     })
 }
@@ -321,15 +362,15 @@ function view1() {
 function view2() {
     new Vue({
         el: '#app',
-        data () {
+        data() {
             return {
                 info2: null
             }
         },
-        mounted () {
+        mounted() {
             axios
                 .get('https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson')
-                .then(response => ( this.info2 = response.data.features ))
+                .then(response => (this.info2 = response.data.features))
         }
     })
 }
@@ -352,17 +393,48 @@ function legenda() {
     var legendTable = new Vue({
         el: '#legendTable',
         data: {
-          items: [
-            { magnitude: '1.0 - 1.9', efect: 'Microearthquakes, not felt, or felt rarely. Recorded by seismographs.'  },
-            { magnitude: '2.0 - 2.9', efect: 'Felt slightly by some people. No damage to buildings.' },
-            { magnitude: '3.0 - 3.9', efect: 'Often felt by people, but very rarely causes damage. Shaking of indoor objects can be noticeable.' },
-            { magnitude: '4.0 - 4.9', efect: 'Noticeable shaking of indoor objects and rattling noises. Felt by most people in the affected area. Slightly felt outside. Generally causes zero to minimal damage. Moderate to significant damage very unlikely. Some objects may fall off shelves or be knocked over.' },
-            { magnitude: '5.0 - 5.9', efect: 'Can cause damage of varying severity to poorly constructed buildings. Zero to slight damage to all other buildings. Felt by everyone.' },
-            { magnitude: '6.0 - 6.9', efect: 'Damage to a moderate number of well-built structures in populated areas. Earthquake-resistant structures survive with slight to moderate damage. Poorly designed structures receive moderate to severe damage. Felt in wider areas; up to hundreds of miles/kilometers from the epicenter. Strong to violent shaking in epicentral area.' },
-            { magnitude: '7.0 - 7.9', efect: 'Causes damage to most buildings, some to partially or completely collapse or receive severe damage. Well-designed structures are likely to receive damage. Felt across great distances with major damage mostly limited to 250 km from epicenter.' },
-            { magnitude: '8.0 - 8.9', efect: 'Major damage to buildings, structures likely to be destroyed. Will cause moderate to heavy damage to sturdy or earthquake-resistant buildings. Damaging in large areas. Felt in extremely large regions.' },
-            { magnitude: '9.0 and more', efect: 'At or near total destruction – severe damage or collapse to all buildings. Heavy damage and shaking extends to distant locations. Permanent changes in ground topography.' }
-          ]
+            items: [
+                { magnitude: '1.0 - 1.9', efect: 'Microearthquakes, not felt, or felt rarely. Recorded by seismographs.' },
+                { magnitude: '2.0 - 2.9', efect: 'Felt slightly by some people. No damage to buildings.' },
+                { magnitude: '3.0 - 3.9', efect: 'Often felt by people, but very rarely causes damage. Shaking of indoor objects can be noticeable.' },
+                { magnitude: '4.0 - 4.9', efect: 'Noticeable shaking of indoor objects and rattling noises. Felt by most people in the affected area. Slightly felt outside. Generally causes zero to minimal damage. Moderate to significant damage very unlikely. Some objects may fall off shelves or be knocked over.' },
+                { magnitude: '5.0 - 5.9', efect: 'Can cause damage of varying severity to poorly constructed buildings. Zero to slight damage to all other buildings. Felt by everyone.' },
+                { magnitude: '6.0 - 6.9', efect: 'Damage to a moderate number of well-built structures in populated areas. Earthquake-resistant structures survive with slight to moderate damage. Poorly designed structures receive moderate to severe damage. Felt in wider areas; up to hundreds of miles/kilometers from the epicenter. Strong to violent shaking in epicentral area.' },
+                { magnitude: '7.0 - 7.9', efect: 'Causes damage to most buildings, some to partially or completely collapse or receive severe damage. Well-designed structures are likely to receive damage. Felt across great distances with major damage mostly limited to 250 km from epicenter.' },
+                { magnitude: '8.0 - 8.9', efect: 'Major damage to buildings, structures likely to be destroyed. Will cause moderate to heavy damage to sturdy or earthquake-resistant buildings. Damaging in large areas. Felt in extremely large regions.' },
+                { magnitude: '9.0 and more', efect: 'At or near total destruction – severe damage or collapse to all buildings. Heavy damage and shaking extends to distant locations. Permanent changes in ground topography.' }
+            ]
         }
-      })
+    })
 }
+
+function onFeatureSelect(feature) {
+    currentSelected = feature.attributes.lon;
+    document.getElementById(feature.attributes.lon).style.color = "#0033cc";
+    document.getElementById(feature.attributes.lon).style.fontWeight = "900";
+    document.getElementById(feature.attributes.lon).style.fontSize = "20px";
+
+    var feature = new OpenLayers.Feature.Vector(
+        new OpenLayers.Geometry.Point(feature.attributes.lon, feature.attributes.lat).transform(epsg4326, projectTo),
+        { lon: feature.attributes.lon, lat: feature.attributes.lat, magnitude: feature.attributes.magnitude },
+        { externalGraphic: 'img/marker2.png', graphicHeight: feature.attributes.magnitude, graphicWidth: feature.attributes.magnitude, graphicXOffset: -12, graphicYOffset: -25 }
+    );
+
+    vectorLayer.addFeatures(feature);
+}
+
+function onFeatureUnselect(feature) {
+    if (currentSelected != undefined) {
+
+        document.getElementById(currentSelected).style.color = "black";
+        document.getElementById(currentSelected).style.fontWeight = "500";
+        document.getElementById(feature.attributes.lon).style.fontSize = "15px";
+        var feature = new OpenLayers.Feature.Vector(
+            new OpenLayers.Geometry.Point(feature.attributes.lon, feature.attributes.lat).transform(epsg4326, projectTo),
+            { lon: feature.attributes.lon, lat: feature.attributes.lat, magnitude: feature.attributes.magnitude },
+            { externalGraphic: 'img/marker.png', graphicHeight: feature.attributes.magnitude, graphicWidth: feature.attributes.magnitude, graphicXOffset: -12, graphicYOffset: -25 }
+        );
+
+        vectorLayer.addFeatures(feature);
+    }
+} 
